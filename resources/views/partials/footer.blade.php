58473a46fd622a19243e8e2ed2d86c79e920d486
hafizhhall <banyuadem8@gmail.com>
2024-02-24 10:28:50 +0700

    Menambahkan  Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,115 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+  {{-- <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media --> --}}
+
+  <!-- Section: Links  -->
+  <section class=" border-top shadow-lg">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4 col-md-2">
+            <i class="fas fa-gem me-3"></i><img src="/img/logoHitam.png" alt="" width="200" class="mr-2">
+          </h6>
+          <p>
+            Anggrek Widarakandang adalah organisasi yang sudah berdiri sejak tahun 1978. Fokus utama dari tempat ini ialah menjual bibit anggrek beserta tanaman anggrek dengan koleksinya yang beragam dari seluruh Indonesia.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4 col-md-6">
+            PRODUK
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Rental</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Pot Plant</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Nutrition</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Flower Arrangment</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4 col-1">SOSIAL MEDIA</h6>
+          <p>
+            <a href="#!" class="text-reset">Instagram</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Facebook</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">YouTube</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">WhatsApp</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4 col-md-2">Contact</h6>
+          <p><i class="fas fa-home me-3"></i>Jl. Hibrida No.10, Muja Muju, Kec. Umbulharjo, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55165</p>
+          <p><i class="fas fa-phone me-3"></i>+62 8960-5007-001</p>
+          <p><i class="fas fa-print me-3"></i>+62 899-5063-877</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="cpy text-center p-4" style="background-color: #0D9276;">
+    © 2024 Copyright:
+    <a class="text-reset fw-bold">ANGGREK WIDARAKANDANG</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
