ded696356c8160a81807c3d01ea95084499c7977
hafizhhall <banyuadem8@gmail.com>
2024-02-25 10:29:44 +0700

    fix banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,45 @@
 @extends('layouts.main')
 
 @section('carousel')
-<div id="carouselExampleCaptions" class="carousel slide carousel-fade">
+<div id="hero-carousel" class="carousel slide" data-bs-slide="carousel">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide1"></button>
+      <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="1" aria-label="Slide2"></button>
+      <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="2" aria-label="Slide3"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active c-item">
-        <img src="/img/1.jpg" class="d-block w-100 c-img" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
+        <img src="/img/1.jpg" class="d-block w-100 c-img" alt="Slide1">
+        <div class="carousel-caption top-0 mt-4 d-none d-md-block">
+            <p class="mt-5 fs-3 text-uppercase">Selamat Datang di!</p>
+            <h1 class="display-1 fw-border text-capitalize">ANGGREK WIDARAKANDANG</h1>
+            <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Lihat Anggrek</button>
         </div>
       </div>
       <div class="carousel-item c-item">
-        <img src="/img/2.jpg" class="d-block w-100 c-img" alt="...">
+        <img src="/img/2.jpg" class="d-block w-100 c-img" alt="Slide2">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
+          <h5>2</h5>
           <p>Some representative placeholder content for the second slide.</p>
         </div>
       </div>
       <div class="carousel-item c-item">
-        <img src="/img/3.jpg" class="d-block w-100 c-img" alt="...">
+        <img src="/img/3.jpg" class="d-block w-100 c-img" alt="Slide3">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
+            <h5>3</h5>
+          <p>Some representative placeholder content for the second slide.</p>
         </div>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#hero-carousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#hero-carousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
 </div>
 @endsection
 
