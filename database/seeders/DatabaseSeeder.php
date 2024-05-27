3cec92d820c39c9cb0d657edce683cf308605cab
hafizhhall <banyuadem8@gmail.com>
2024-05-27 13:56:56 +0700

    confirm delete

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,62 +20,62 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'banyu adem',
-            'email' => 'banyu@gmail.com',
-            'role' => 'admin',
-            'password' => bcrypt('password')
-        ]);
-        User::create([
-            'name' => 'athallah',
-            'email' => 'athallah@gmail.com',
-            'role' => 'pelanggan',
-            'password' => bcrypt('password')
-        ]);
-        User::create([
-            'name' => 'widianto',
-            'email' => 'widianto@gmail.com',
-            'role' => 'pemilik',
-            'password' => bcrypt('password')
-        ]);
+        // User::create([
+        //     'name' => 'banyu adem',
+        //     'email' => 'banyu@gmail.com',
+        //     'role' => 'admin',
+        //     'password' => bcrypt('password')
+        // ]);
+        // User::create([
+        //     'name' => 'athallah',
+        //     'email' => 'athallah@gmail.com',
+        //     'role' => 'pelanggan',
+        //     'password' => bcrypt('password')
+        // ]);
+        // User::create([
+        //     'name' => 'widianto',
+        //     'email' => 'widianto@gmail.com',
+        //     'role' => 'pemilik',
+        //     'password' => bcrypt('password')
+        // ]);
         // User::factory(5)->create();
-        // Artikel::factory(20)->create();
-        Kategori::create([
-            'nama' => 'Prestasi',
-            'slug' => 'prestasi'
-        ]);
-        Kategori::create([
-            'nama' => 'Informasi',
-            'slug' => 'informasi'
-        ]);
-        Kategori::create([
-            'nama' => 'Kegiatan',
-            'slug' => 'kegiatan'
-        ]);
-        Jenis::create([
-            'name' => 'Phalaenopsis',
-            'slug' => 'phalaenopsis'
-        ]);
-        Jenis::create([
-            'name' => 'Dendrobium',
-            'slug' => 'dendrobium'
-        ]);
-        Jenis::create([
-            'name' => 'Vanda',
-            'slug' => 'vanda'
-        ]);
-        Jenis::create([
-            'name' => 'Oncidium',
-            'slug' => 'oncidium'
-        ]);
+        Artikel::factory(10)->create();
+        // Kategori::create([
+        //     'nama' => 'Prestasi',
+        //     'slug' => 'prestasi'
+        // ]);
+        // Kategori::create([
+        //     'nama' => 'Informasi',
+        //     'slug' => 'informasi'
+        // ]);
+        // Kategori::create([
+        //     'nama' => 'Kegiatan',
+        //     'slug' => 'kegiatan'
+        // ]);
+        // Jenis::create([
+        //     'name' => 'Phalaenopsis',
+        //     'slug' => 'phalaenopsis'
+        // ]);
+        // Jenis::create([
+        //     'name' => 'Dendrobium',
+        //     'slug' => 'dendrobium'
+        // ]);
+        // Jenis::create([
+        //     'name' => 'Vanda',
+        //     'slug' => 'vanda'
+        // ]);
+        // Jenis::create([
+        //     'name' => 'Oncidium',
+        //     'slug' => 'oncidium'
+        // ]);
 
-        Supplier::create([
-            'perusahaan' => 'Mikum jaya anggrek',
-            'slug' => 'mikum-jaya-anggrek',
-            'nama' => 'Wigunan',
-            'phone' => '0899889899',
-            'note' => 'Supplier ini sangat baik dan suka datang jam 3 sore setiap hari selasa pagi'
-        ]);
+        // Supplier::create([
+        //     'perusahaan' => 'Mikum jaya anggrek',
+        //     'slug' => 'mikum-jaya-anggrek',
+        //     'nama' => 'Wigunan',
+        //     'phone' => '0899889899',
+        //     'note' => 'Supplier ini sangat baik dan suka datang jam 3 sore setiap hari selasa pagi'
+        // ]);
 
     }
 }
--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -39,7 +39,6 @@
         @include('dashboard.layouts.sidebar')
 
         @yield('content')
-
         @include('dashboard.layouts.setting')
 
         <footer>
