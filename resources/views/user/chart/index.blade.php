df450d076569b10dc8e28b1d14c392757482e2dd
hafizhhall <banyuadem8@gmail.com>
2024-07-08 10:28:11 +0700

    fix layout cart

--- resources/views/user/chart/index.blade.php
+++ resources/views/user/chart/index.blade.php
@@ -110,15 +110,14 @@
                             </tbody>
                         </table>
                     </div>
-                    <div class="row g-4 justify-content-end mt-5">
+                    <div class="row mt-5">
                         <div class="col-md-8">
                             <h5>Pilih alamat pengiriman</h5>
-
-                            <div class="row">
+                            <div class="row d-flex">
                                 <form action="{{ route('chart.updateShippingAddress') }}" method="POST">
                                     @csrf
                                     @foreach ($address as $alamat)
-                                        <div class="col-md-6 mt-2">
+                                        <div class="col-md-12 mt-2">
                                             <div class="card p-3">
                                                 <address>
                                                     <input class="form-check-input delivery-address" type="radio"
@@ -151,7 +150,7 @@
                                             <label for="new_name" class="form-label">Nama Lengkap</label>
                                             <input type="text"
                                                 name="new_name"class="form-control @error('new_name')
-                                            @enderror"
+                                                @enderror"
                                                 placeholder="" value="{{ old('new_name') }}">
                                             @error('new_name')
                                                 <div class="invalid-feedback">
@@ -163,8 +162,8 @@
                                             <label for="new_phone" class="form-label">Nomor telepon</label>
                                             <input type="text"
                                                 name="new_phone"class="form-control @error('new_phone')
-                                                is-invalid
-                                            @enderror"
+                                                    is-invalid
+                                                @enderror"
                                                 placeholder="" value="{{ old('new_phone') }}">
                                             @error('new_phone')
                                                 <div class="invalid-feedback">
@@ -197,8 +196,8 @@
                                             <label for="new_pos" class="form-label">Kode POS</label>
                                             <input type="text" name="new_pos"
                                                 class="form-control @error('new_pos')
-                                                is-invalid
-                                            @enderror"placeholder=""
+                                                    is-invalid
+                                                @enderror"placeholder=""
                                                 value="{{ old('new_pos') }}">
                                             @error('new_pos')
                                                 <div class="invalid-feedback">
@@ -208,7 +207,11 @@
                                         </div>
                                         <div class="mt-2">
                                             <label for="new_alamat" class="form-label">Alamat lengkap</label>
-                                            <textarea name="new_alamat" id="new_alamat" cols="30" rows="3" class="form-control">@if(old('new_alamat')){{ old('new_alamat') }}@endif</textarea>
+                                            <textarea name="new_alamat" id="new_alamat" cols="30" rows="3" class="form-control">
+@if (old('new_alamat'))
+{{ old('new_alamat') }}
+@endif
+</textarea>
                                             @error('new_alamat')
                                                 <div class="invalid-feedback">
                                                     {{ $message }}
@@ -218,63 +221,62 @@
                                         <div class="col-md-4">
                                             <button type="submit" class="btn btn-primary mt-3">Simpan Alamat
                                                 Pengiriman</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="bg-light rounded-3">
+                                <div class="p-4">
+                                    <h1 class="display-6 mb-4">Cart <span class="fw-normal">Total</span></h1>
+                                    <div class="d-flex justify-content-between mb-4">
+                                        <h5 class="mb-0 me-4">Subtotal:</h5>
+                                        <p class="mb-0">Rp{{ number_format($total, 0, ',', '.') }}</p>
+                                    </div>
+                                </div>
+                                <div class="py-4 mb-4 border-top border-bottom d-flex justify-content-between">
+                                    <h5 class="mb-0 ps-4 me-4">Total</h5>
+                                    <p class="mb-0 pe-4">Rp{{ number_format($total, 0, ',', '.') }}</p>
+                                </div>
+                                <form action="/checkout" method="post">
+                                    @csrf
+                                    <input type="hidden" name="totalBerat" value="{{ $totalBerat }}">
+                                    <input type="hidden" name="total" value="{{ $total }}">
+                                    <input type="hidden" name="shipping_address_id" id="selected_shipping_address_id">
+                                    <input type="hidden" name="courier_code" id="selected_courier_code">
+                                    <button
+                                        class="btn border-secondary rounded-pill px-4 py-3 text-primary text-uppercase mb-4 ms-4"
+                                        type="submit">Checkout</button>
                                 </form>
                             </div>
                         </div>
                     </div>
+
                     <div class="row">
                         <div class="col-md-8">
                             <div class="mt-3">
                                 <h5>Pilih kurir pengiriman</h5>
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input courier-code" type="radio" name="courier_code" id="courier_jne" value="jne">
+                                    <input class="form-check-input courier-code" type="radio" name="courier_code"
+                                        id="courier_jne" value="jne">
                                     <label for="inlineRadio1" class="form-check-label">JNE</label>
                                 </div>
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input courier-code" type="radio" name="courier_code" id="courier_tiki" value="tiki">
+                                    <input class="form-check-input courier-code" type="radio" name="courier_code"
+                                        id="courier_tiki" value="tiki">
                                     <label for="inlineRadio1" class="form-check-label">TIKI</label>
                                 </div>
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input courier-code" type="radio" name="courier_code" id="courier_pos" value="pos">
+                                    <input class="form-check-input courier-code" type="radio" name="courier_code"
+                                        id="courier_pos" value="pos">
                                     <label for="inlineRadio1" class="form-check-label">POS</label>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    {{-- <div class="row">
-                        <div class="">
-                            <p>Layanan tersedia</p>
-                            <ul class="list-group list-group-flush available-service" style="display: none">
+                </div>
 
-                            </ul>
-                        </div>
-                    </div> --}}
-                </div>
-                <div class="col-md-4">
-                    <div class="bg-light rounded-3">
-                        <div class="p-4">
-                            <h1 class="display-6 mb-4">Cart <span class="fw-normal">Total</span></h1>
-                            <div class="d-flex justify-content-between mb-4">
-                                <h5 class="mb-0 me-4">Subtotal:</h5>
-                                <p class="mb-0">Rp{{ number_format($total, 0, ',', '.') }}</p>
-                            </div>
-                        </div>
-                        <div class="py-4 mb-4 border-top border-bottom d-flex justify-content-between">
-                            <h5 class="mb-0 ps-4 me-4">Total</h5>
-                            <p class="mb-0 pe-4">Rp{{ number_format($total, 0, ',', '.') }}</p>
-                        </div>
-                        <form action="/checkout" method="post">
-                            @csrf
-                            <input type="hidden" name="totalBerat" value="{{ $totalBerat }}">
-                            <input type="hidden" name="total" value="{{ $total }}">
-                            <input type="hidden" name="shipping_address_id" id="selected_shipping_address_id">
-                            <input type="hidden" name="courier_code" id="selected_courier_code">
-                            <button
-                                class="btn border-secondary rounded-pill px-4 py-3 text-primary text-uppercase mb-4 ms-4"
-                                type="submit">Checkout</button>
-                        </form>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
